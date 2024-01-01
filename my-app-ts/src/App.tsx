6a479a0a6bc9173b1db576402b4b2753632ec590
BrunoTrindade40 <brunotrindade_40@hotmail.com>
2024-01-01 19:37:25 -0300

    primeiro

---
+++ my-app-ts/src/App.tsx
@@ -0,0 +1,31 @@
+import { Card } from "./components/Card";
+import { Footer } from "./components/Footer";
+import { Header } from "./components/Header";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <div className="App">
+        <Card
+          id={1}
+          paragraph="Typescript"
+          details="TS para frontend e backend"
+        />
+        <Card
+          id={2}
+          paragraph="HTML"
+          details="HTML para frontend"
+        />
+        <Card
+          id={3}
+          paragraph="SQL"
+          details="SQL para banco de dados"
+        />
+      </div>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
